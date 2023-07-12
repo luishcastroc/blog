978eefc27d2bbc749ef25e5e733ad796586144fb
Luis Castro <luish.castroc@gmail.com>
2023-07-12 19:35:29 -0400

    chore(style): add animation and css fixes

--- blog/src/app/components/dashboard.component.ts
+++ blog/src/app/components/dashboard.component.ts
@@ -1,3 +1,4 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import {
   RouterLinkActive,
@@ -8,18 +9,17 @@ import {
 @Component({
   selector: 'blog-dashboard',
   standalone: true,
-  imports: [RouterOutlet, RouterLinkWithHref, RouterLinkActive],
+  imports: [RouterOutlet, RouterLinkWithHref, RouterLinkActive, NgFor],
   host: {
     class: 'flex min-h-screen flex-col',
   },
   template: `
-    <main class="flex flex-col flex-auto pt-6">
+    <main class="flex flex-col flex-auto pt-6 absolute w-full z-10">
       <nav class="navbar bg-primary text-primary-content max-h-20">
         <div class="navbar-start justify-end mr-3">
           <ul class="menu menu-horizontal px-1 text-base gap-8">
             <li
-              class="relative text-xl w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center"
-            >
+              class="relative text-xl w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:[&:not(&:has(a.active))]:scale-x-100 after:transition after:duration-300 after:origin-center">
               <a
                 routerLink="/home"
                 routerLinkActive="active"
@@ -29,8 +29,7 @@ import {
               >
             </li>
             <li
-              class="relative text-xl w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center"
-            >
+              class="relative text-xl w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:[&:not(&:has(a.active))]:scale-x-100 after:transition after:duration-300 after:origin-center">
               <a
                 routerLink="/blog"
                 routerLinkActive="active"
@@ -40,8 +39,7 @@ import {
               >
             </li>
             <li
-              class="relative text-xl w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center"
-            >
+              class="relative text-xl w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:[&:not(&:has(a.active))]:scale-x-100 after:transition after:duration-300 after:origin-center">
               <a
                 routerLink="/about"
                 routerLinkActive="active"
@@ -51,8 +49,7 @@ import {
               >
             </li>
             <li
-              class="relative text-xl w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center"
-            >
+              class="relative text-xl w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:[&:not(&:has(a.active))]:scale-x-100 after:transition after:duration-300 after:origin-center">
               <a
                 routerLink="/contact"
                 routerLinkActive="active"
@@ -66,8 +63,7 @@ import {
         <div class="navbar-center mr-4 ml-4">
           <div class="avatar">
             <div
-              class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 hover:ring-secondary-focus hover:w-28 hover:h-auto hover:transition-all"
-            >
+              class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 hover:ring-secondary-focus hover:w-28 hover:h-auto hover:transition-all">
               <a routerLink="/home"
                 ><img src="assets/logo.svg" width="80" height="80"
               /></a>
@@ -117,6 +113,13 @@ import {
       </nav>
       <router-outlet></router-outlet>
     </main>
+    <div class="circle-container">
+      <ul class="circles">
+        <li *ngFor="let number of numbers"></li>
+      </ul>
+    </div>
   `,
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+}
