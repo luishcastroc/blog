47c925ec7415dd0447a5e75250b1063284cf0b68
Luis Castro <luish.castroc@gmail.com>
2023-07-13 15:21:47 -0400

    chore(blog): add animation to theme change

--- blog/src/app/components/dashboard.component.ts
+++ blog/src/app/components/dashboard.component.ts
@@ -1,11 +1,11 @@
-import { NgFor } from '@angular/common';
-import { ChangeDetectorRef, Component, Renderer2, inject } from '@angular/core';
+import { NgClass, NgFor } from '@angular/common';
+import { Component, Renderer2, inject } from '@angular/core';
 import {
   RouterLinkActive,
   RouterLinkWithHref,
   RouterOutlet,
 } from '@angular/router';
-import { SvgIconComponent, SvgIcons } from '@ngneat/svg-icon';
+import { SvgIconComponent } from '@ngneat/svg-icon';
 
 @Component({
   selector: 'blog-dashboard',
@@ -16,6 +16,7 @@ import { SvgIconComponent, SvgIcons } from '@ngneat/svg-icon';
     RouterLinkActive,
     NgFor,
     SvgIconComponent,
+    NgClass,
   ],
   host: {
     class: 'flex min-h-screen flex-col',
@@ -72,40 +73,79 @@ import { SvgIconComponent, SvgIcons } from '@ngneat/svg-icon';
             <div
               class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 hover:ring-secondary-focus hover:w-28 hover:h-auto hover:transition-all">
               <a routerLink="/home"
-                ><img src="assets/logo.svg" width="80" height="80"
+                ><img
+                  src="assets/logo.svg"
+                  width="80"
+                  height="80"
+                  aria-label="logo"
               /></a>
             </div>
           </div>
         </div>
         <div class="navbar-end">
           <ul class="menu menu-horizontal px-1 text-base gap-8">
-            <li>
+            <li class="w-16">
               <a
                 href="https://twitter.com/LuisHCCDev"
                 target="_blank"
                 rel="noreferrer noopener"
-                ><svg-icon key="twitter" fontSize="30px" height="30px"
+                ><svg-icon
+                  key="twitter"
+                  fontSize="30px"
+                  height="30px"
+                  aria-label="twitter"
               /></a>
             </li>
-            <li>
+            <li class="w-16">
               <a
                 href="https://github.com/luishcastroc"
                 target="_blank"
                 rel="noreferrer noopener"
-                ><svg-icon key="github" fontSize="30px" height="30px"
+                ><svg-icon
+                  key="github"
+                  fontSize="30px"
+                  height="30px"
+                  aria-label="github"
               /></a>
             </li>
-            <li>
+            <li class="w-16">
               <a
                 href="https://www.threads.net/@luishccdev"
                 target="_blank"
                 rel="noreferrer noopener"
-                ><svg-icon key="threads" fontSize="30px" height="30px"
+                ><svg-icon
+                  key="threads"
+                  fontSize="30px"
+                  height="30px"
+                  aria-label="threads"
               /></a>
             </li>
-            <li>
-              <button class="btn btn-square btn-ghost" (click)="changeTheme()">
-                <svg-icon [key]="icon" fontSize="30px" height="30px" />
+            <li class="w-16">
+              <button
+                class="btn btn-square btn-ghost w-full h-[46px] relative overflow-hidden"
+                (click)="changeTheme()">
+                <svg-icon
+                  class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                  [ngClass]="{
+                    'opacity-0 transition-all translate-y-[20%] rotate-[50deg] ease-out duration-1000': isDarkMode,
+                    'opacity-[1] transition-all ease-out duration-1000': !isDarkMode,
+                  }"
+                  key="dark-mode"
+                  fontSize="30px"
+                  height="30px"
+                  aria-label="Chage theme to dark" />
+                <svg-icon
+                  class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                  [ngClass]="{
+                    'opacity-[1] transition-all ease-out duration-1000':
+                      isDarkMode,
+                    'opacity-0 transition-all translate-y-[20%] rotate-[100deg]':
+                      !isDarkMode
+                  }"
+                  key="light"
+                  fontSize="30px"
+                  height="30px"
+                  aria-label="Change theme to light" />
               </button>
             </li>
           </ul>
@@ -113,6 +153,7 @@ import { SvgIconComponent, SvgIcons } from '@ngneat/svg-icon';
       </nav>
       <router-outlet></router-outlet>
     </main>
+    <!-- Animated circles container -->
     <div class="circle-container">
       <ul class="circles">
         <li *ngFor="let number of numbers"></li>
@@ -122,19 +163,17 @@ import { SvgIconComponent, SvgIcons } from '@ngneat/svg-icon';
 })
 export class DashboardComponent {
   #renderer = inject(Renderer2);
-  #cdr = inject(ChangeDetectorRef);
-  icon: SvgIcons = 'dark-mode';
   numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  isDarkMode = false;
 
   changeTheme() {
     const body = this.#renderer.selectRootElement('body', true) as HTMLElement;
     if (body.getAttribute('data-theme') === 'dark') {
-      this.icon = 'dark-mode';
       body.setAttribute('data-theme', 'bumblebee');
+      this.isDarkMode = false;
     } else {
-      this.icon = 'light';
       body.setAttribute('data-theme', 'dark');
+      this.isDarkMode = true;
     }
-    this.#cdr.detectChanges();
   }
 }
--- blog/tailwind.config.js
+++ blog/tailwind.config.js
@@ -3,6 +3,7 @@ const { join } = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
