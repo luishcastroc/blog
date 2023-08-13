b5393cae2dac245eecd3bdd7f258ad0fb8bc3e5c
Luis Castro <luish.castroc@gmail.com>
2023-08-13 09:12:03 -0400

    chore(blog): fix some styles

--- blog/src/app/pages/blog/index.page.ts
+++ blog/src/app/pages/blog/index.page.ts
@@ -17,7 +17,7 @@ export const routeMeta: RouteMeta = {
   imports: [RouterLink, NgFor, JsonPipe, BlogCoverComponent],
   template: `
     <div
-      class="flex flex-col flex-wrap gap-8 overflow-auto lg:flex-row lg:gap-6 ">
+      class="flex flex-col flex-wrap justify-center gap-8 overflow-auto lg:flex-row lg:gap-6 ">
       <ng-container *ngFor="let post of posts">
         <blog-cover [post]="post" />
       </ng-container>
