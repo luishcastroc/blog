b19f4154699a20d26cda848b34ea523b060d1792
Luis Castro <luish.castroc@gmail.com>
2023-08-22 08:33:37 -0400

    fix(blog): add padding to cards for better looking

--- blog/src/app/pages/blog/index.page.ts
+++ blog/src/app/pages/blog/index.page.ts
@@ -17,7 +17,7 @@ export const routeMeta: RouteMeta = {
   imports: [RouterLink, NgFor, JsonPipe, BlogCoverComponent],
   template: `
     <div
-      class="flex flex-col flex-wrap justify-center gap-8 overflow-auto lg:flex-row lg:gap-6 ">
+      class="flex flex-col flex-wrap justify-center gap-8 overflow-auto pb-4 pt-4 lg:flex-row lg:gap-6 ">
       <ng-container *ngFor="let post of posts">
         <blog-cover [post]="post" />
       </ng-container>
