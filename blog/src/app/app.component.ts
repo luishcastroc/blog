1ad493324e3f3d3d29c9c2b9424cb4213e21ec28
Luis Castro <luish.castroc@gmail.com>
2023-09-08 14:50:54 -0400

    chore(i18N): add meta changes when lang changes

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { DashboardComponent } from './components/dashboard.component';
+import { TranslocoService } from '@ngneat/transloco';
+import { DOCUMENT } from '@angular/common';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'blog-root',
@@ -7,4 +10,30 @@ import { DashboardComponent } from './components/dashboard.component';
   imports: [DashboardComponent],
   template: ` <mr-dashboard /> `,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  #document = inject(DOCUMENT);
+  #meta = inject(Meta);
+  #transloco = inject(TranslocoService);
+
+  ngOnInit(): void {
+    this.#transloco.langChanges$.subscribe(lang => {
+      this.#document.documentElement.lang = lang;
+      const metaDescription = this.#transloco.translate('meta.description');
+      const twitterDescription = this.#transloco.translate(
+        'meta.twitter-description'
+      );
+      if (metaDescription && metaDescription !== 'meta.description') {
+        this.#meta.updateTag({ name: 'description', content: metaDescription });
+      }
+      if (
+        twitterDescription &&
+        twitterDescription !== 'meta.twitter-description'
+      ) {
+        this.#meta.updateTag({
+          name: 'twitter:description',
+          content: twitterDescription,
+        });
+      }
+    });
+  }
+}
