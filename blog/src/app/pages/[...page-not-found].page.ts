9c4ea9e99174dd3211a6891c658a11454f80e98c
Luis Castro <luish.castroc@gmail.com>
2023-09-08 22:39:11 -0400

    chore(i18n): add missing translation

--- blog/src/app/pages/[...page-not-found].page.ts
+++ blog/src/app/pages/[...page-not-found].page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { RouteMeta } from '@analogjs/router';
 import { RouterLink } from '@angular/router';
+import { TranslocoModule } from '@ngneat/transloco';
 
 export const routeMeta: RouteMeta = {
   title: 'Luis Castro - Page Not Found',
@@ -9,18 +10,20 @@ export const routeMeta: RouteMeta = {
 
 @Component({
   standalone: true,
-  imports: [RouterLink, NgOptimizedImage],
-  template: `
+  imports: [RouterLink, NgOptimizedImage, TranslocoModule],
+  template: `<ng-container *transloco="let t">
     <div class="relative flex flex-auto items-center justify-center">
       <div class="flex flex-col items-center justify-items-center">
         <img
-          ngSrc="assets/not-found.svg"
+          ngSrc="v1694226664/not-found.svg"
           alt="Not Found Image"
           width="566"
           height="400" />
-        <button routerLink="/" class="btn btn-primary">Take me Home!!</button>
+        <button routerLink="/" class="btn btn-primary">
+          {{ t('take-me-home') }}
+        </button>
       </div>
     </div>
-  `,
+  </ng-container> `,
 })
 export default class NotFoundPage {}
